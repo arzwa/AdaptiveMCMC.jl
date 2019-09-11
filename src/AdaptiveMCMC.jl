c90fb9129f6a6bf7ae27886c58054436024a6de7
arzwa <arthur.zwaenepoel@gmail.com>
2019-09-11 13:25:19 +0200

    Update travis.yml

--- src/AdaptiveMCMC.jl
+++ src/AdaptiveMCMC.jl
@@ -1,21 +1,21 @@
 module AdaptiveMCMC
 
-using Distributions
+    using Distributions
 
-include("proposals.jl")
+    include("proposals.jl")
 
-"""
-    Chain
+    """
+        Chain
 
-Subtypes of Chain are assumed to have a `state` and `proposals` field, and
-should have a `logpdf` method that can take a variable number of pairs as
-arguments.
-"""
-abstract type Chain end
+    Subtypes of Chain are assumed to have a `state` and `proposals` field, and
+    should have a `logpdf` method that can take a variable number of pairs as
+    arguments.
+    """
+    abstract type Chain end
 
 
-export
-    Proposals, AdaptiveUvProposal, AdaptiveRwProposal, AdaptiveUnProposal
-    adapt!, rw, scale, reflect
+    export
+        Proposals, AdaptiveUvProposal, AdaptiveRwProposal, AdaptiveUnProposal,
+        adapt!, rw, scale, reflect
 
 end
